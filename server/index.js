33895e617715249a01ab6e282ccd2c417db6dec7
Dagmar <75873640+dagmar-dev@users.noreply.github.com>
2023-05-29 18:17:34 -0700

    set open ai api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+require('dotenv').config()
+const { Configuration, OpenAIApi } = require('openai')
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 app.use(cors())
 
@@ -11,8 +17,13 @@ app.use(
     })
 )
 
-app.get('/', (req, res) => {
-     res.json({ msg: 'This is CORS-enabled for only example.com.' })
+app.post('/', async (req, res) => {
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: 'Hello world' }],
+    })
+    res.json(completion.data.choices[0].message)
+    console.log(completion.data.choices[0].message)
 })
 
 app.listen(port, () => {
