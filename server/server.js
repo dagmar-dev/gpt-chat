a690b2a32aca84d1622326a7fb0eb61524301c40
Dagmar <dagmarlewis8@gmail.com>
2023-07-18 17:02:14 -0700

    comment unused dependences

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const mongoose = require("mongoose")
-const passport = require("passport");
-const session = require("express-session")
+// const mongoose = require("mongoose")
+// const passport = require("passport");
+// const session = require("express-session")
 const port = 3300
 const cors = require('cors')
 require('dotenv').config()
