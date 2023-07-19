67eca8d1c7817b60fd73c50c6f8bd8586d9a55c9
Dagmar <dagmarlewis8@gmail.com>
2023-07-18 17:55:01 -0700

    attempt to fix mongoose

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,31 +1,31 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const MessagesSchema = new mongoose.Schema({
-    messages : [
-        title= {
-        type:String,
-        required:true
-    },
-            role= {
-        type:String,
-        required:true
-    },
-    message = {
-        type:String,
-        required:true
-    },
+// const MessagesSchema = new mongoose.Schema({
+//     messages : [
+//         title= {
+//         type:String,
+//         required:true
+//     },
+//             role= {
+//         type:String,
+//         required:true
+//     },
+//     message = {
+//         type:String,
+//         required:true
+//     },
     
-    ],
+//     ],
     
-    user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+//     user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User",
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
 
-})
+// })
 
-module.exports = mongoose.model("Messages")
+// module.exports = mongoose.model("Messages")
