87b270b2b33fb0c133c53fe12608853c3c966897
Dagmar <dagmarlewis8@gmail.com>
2023-08-17 02:14:42 -0400

    update axios

--- client/src/api/config/axiosConfig.js
+++ client/src/api/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://gpt-server-c3mk.onrender.com:3300',
+    baseURL:'https://gpt-server-c3mk.onrender.com:3300',
 })
 
 // defining a custom error handler for all APIs
