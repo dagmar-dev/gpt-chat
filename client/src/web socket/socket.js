c8eb7d5780e0b529fa00ab7441225de41c105200
Dagmar <dagmarlewis8@gmail.com>
2023-08-21 21:03:37 -0400

    updated link

--- client/src/web socket/socket.js
+++ client/src/web socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:3300')
+export const socket = io('https://gpt-server-c3mk.onrender.com')
 
 // http://localhost:3300
