a5d820c9925e4f8aeb7e552a79c3ebc2b4f00800
Dagmar Lewis <dagmarlewis8@gmail.com>
2023-09-05 16:11:16 -0500

    removed dev double message fix from prod

--- client/src/components/ChatArea.jsx
+++ client/src/components/ChatArea.jsx
@@ -65,11 +65,13 @@ export default function ChatArea(state) {
     useEffect(() => {
         function onConnect() {
             updateStatus('connected', state)
-            socket.on('response', (data) => {
-                updateStatus('connected', state)
-                addMessage('assistant', data)
-            })
+            
         }
+
+        socket.on('response', (data) => {
+            updateStatus('connected', state)
+            addMessage('assistant', data)
+        })
 
         // socket.on('loading', (data) => {
         //     setLoading(`${data}`)
