6543489ddc0ab9db26e42451765a40756eb15a87
Dagmar Lewis <dagmarlewis8@gmail.com>
2024-09-06 14:37:12 -0500

    integrated posthog

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,24 @@ import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { PostHogProvider } from 'posthog-js/react'
 
 const queryClient = new QueryClient()
+
+const options = {
+    api_host:!import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
-                <App />
+                <PostHogProvider
+                    apiKey={!import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+                    options={options}
+                >
+                    <App />
+                </PostHogProvider>
                 <ReactQueryDevtools initialIsOpen />
             </QueryClientProvider>
         </BrowserRouter>
